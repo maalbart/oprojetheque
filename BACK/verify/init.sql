af381638258e5731da67f66c0a52487274732ceb
SYB-SFLA <sophie.bayram@gmail.com>
2021-10-26 20:53:42 +0200

    omission of the supervise table in the init.sql file in the verify folder

--- BACK/verify/init.sql
+++ BACK/verify/init.sql
@@ -1,10 +1,12 @@
--- Verify oprojetheque:init on pg
+-- Verify oprojetheque:init on PG
 
 BEGIN;
 
-SELECT id,name ,logo ,starting_date ,ending_date FROM promo WHERE false;
-SELECT id, firstname, lastname, email, password, biography, avatar FROM student WHERE false;
-SELECT id, name, logo, description, site_link, site_screen, youtube_link FROM project WHERE false;
-SELECT id,firstname, lastname, email, password, job_title, avatar FROM administrator WHERE false;
+-- -- the WHERE false is a little trick to avoid getting/ recover all the data
+SELECT id, name, logo, starting_date, ending_date FROM promo WHERE false;
+SELECT id, firstname, lastname, email, password, biography, avatar, id_promo, id_project FROM student WHERE false;
+SELECT id, name, logo, description, site_link, site_screen, youtube_link, id_promo FROM project WHERE false;
+SELECT id, firstname, lastname, email, password, job_title, avatar FROM administrator WHERE false;
+SELECT id_administrator, id_promo FROM supervise WHERE false;
 
 ROLLBACK;
