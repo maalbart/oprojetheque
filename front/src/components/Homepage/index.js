106d5d93149baf97a88f61cfd3c454e00106a2c7
Emilien-Boucherie <emilien.boucherie@laposte.net>
2021-10-27 22:09:22 +0200

    fix some style, more responsive reaction

--- front/src/components/Homepage/index.js
+++ front/src/components/Homepage/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 import logo from 'src/assets/logo-principal.png'
 import CardProject from "src/components/CardProject";
 import './style.scss';
@@ -12,11 +13,18 @@ export default function Homepage () {
         Possimus cumque et numquam deleniti quas accusamus itaque. Error nemo, totam vitae ipsam ea quo rerum sint eveniet sed dolorem labore natus expedita culpa! Dolor quis fugiat dolore culpa.</p>
       </div>
       <div className="homepage-suggest">
-        <CardProject title="test1" description="c'est le test 1"/>
-        <CardProject title="test2" description="c'est le test 2"/>
-        <CardProject title="test3" description="c'est le test 3"/>
-        <CardProject title="test4" description="c'est le test 4"/>
-        <CardProject title="test5" description="c'est le test 5"/>
+        <Card.Group 
+          itemsPerRow={5}
+          stackable
+          centered
+          className="homepage-suggest-cards"
+        >
+          <CardProject title="test1" description="c'est le test 1"/>
+          <CardProject title="test2" description="c'est le test 2"/>
+          <CardProject title="test3" description="c'est le test 3"/>
+          <CardProject title="test4" description="c'est le test 4"/>
+          <CardProject title="test5" description="c'est le test 5"/>
+        </Card.Group>
       </div>
     </div>
   )
