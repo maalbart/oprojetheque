7155f3ba66bf52a29b6b9fa89090a14f5f186eb2
Emilien-Boucherie <emilien.boucherie@laposte.net>
2021-10-28 10:33:58 +0200

    component promos done

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -2,13 +2,15 @@
 import './style.scss';
 import Header from 'src/components/Header';
 import Homepage from 'src/components/Homepage';
+import Promos from 'src/components/Promos';
 import Footer from 'src/components/Footer';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
-    <Homepage />
+    {/* <Homepage /> */}
+    <Promos />
     <Footer />
   </div>
 );
