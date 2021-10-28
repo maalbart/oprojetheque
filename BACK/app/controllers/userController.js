568ba7692bd5cc0e204243a16840925e63ae43db
celiatoc <celiatocqueville@gmail.com>
2021-10-28 14:56:49 +0200

    start of middleware admin

--- BACK/app/controllers/userController.js
+++ BACK/app/controllers/userController.js
@@ -2,21 +2,29 @@
     - brypt
     saltRounds (permet de hasher le mot de passe autant de fois qu'on le souhaite )
 */
-const Student = require ("../models/students.js");
+const User = require ("../models/USER.js");
 
 const userController = {
     /* Method to display the home page */
-    getAllStudents:async(req,res)=>{
-        console.log("Bienvenue sur la page des projets");
-        const students = await Student.getAllStudents();
-        res.json(students);
+    // getAllStudents:async(req,res)=>{
+    //     console.log("Bienvenue sur la page des projets");
+    //     const students = await Student.getAllStudents();
+    //     res.json(students);
+    // },
+    // getOneStudent:async(req,res)=>{
+    //     console.log("Bienvenue sur la page d'une promo");
+    //     const studentId = await Student.getOneStudent(req.params.id);
+    //     console.log("projectId", studentId);
+    //     res.json(studentId);
+    //  }
+    login: (req, res, next)=>{
+        res.render("connexion/login");
     },
-    getOneStudent:async(req,res)=>{
-        console.log("Bienvenue sur la page d'une promo");
-        const studentId = await Student.getOneStudent(req.params.id);
-        console.log("projectId", studentId);
-        res.json(studentId);
-     }
+    signUp: (req, res, next)=>{
+        res.render("connexion/signup");
+    },
+    
+
 
 };
 
