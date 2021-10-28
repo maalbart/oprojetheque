f15c973f199f1f651038907d9a47e2eb19bfbd58
Emilien-Boucherie <emilien.boucherie@laposte.net>
2021-10-28 10:46:34 +0200

    component projects done

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -4,13 +4,15 @@ import Header from 'src/components/Header';
 import Homepage from 'src/components/Homepage';
 import Promos from 'src/components/Promos';
 import Footer from 'src/components/Footer';
+import Projects from 'src/components/Projects';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
     {/* <Homepage /> */}
-    <Promos />
+    {/* <Promos /> */}
+    <Projects />
     <Footer />
   </div>
 );
