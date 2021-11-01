523d57c1e80f4bb6107ddb106f20f28fbeb79c4d
celiatoc <celiatocqueville@gmail.com>
2021-11-01 11:09:40 +0100

    good deploy cors

--- BACK/index.js
+++ BACK/index.js
@@ -18,13 +18,19 @@ app.use(router);
 /* Authorization to access the API for the whole world*/
 // const cors = require('cors');
 // app.use(cors({origin: '*'}));
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:8080/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const cors = require('cors');
+// const corsOptions ={
+//     origin:'http://localhost:8080/', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 /* ------------------------------------- */
 /* Access rights agreement to the information of a POST via req.body -
 The extented to false allows to receive only values of type string or array. 
