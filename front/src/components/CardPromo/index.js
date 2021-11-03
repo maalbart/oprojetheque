e4b73606e119dc218650908b4bef54db8ad60489
Emilien-Boucherie <emilien.boucherie@laposte.net>
2021-11-03 15:56:05 +0100

    rework card promo/project

--- front/src/components/CardPromo/index.js
+++ front/src/components/CardPromo/index.js
@@ -1,9 +1,10 @@
 import { Card, Image } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 
-export default function CardProject ({ 
+export default function CardPromo ({ 
   name, 
-  description, 
+  starting_date, 
+  ending_date,
   logo,
 }) {
   return (
@@ -11,16 +12,17 @@ export default function CardProject ({
       <Image src={logo} wrapped ui={false} />
       <Card.Content>
         <Card.Header>{name}</Card.Header>
-          <Card.Description>
-            {description}
-          </Card.Description>
+          <Card.Meta>
+            <span className='date'>{starting_date} - {ending_date}</span>
+          </Card.Meta>
       </Card.Content>
     </Card>
   )
 }
 
-CardProject.propTypes = {
+CardPromo.propTypes = {
   name: PropTypes.string.isRequired,
-  description: PropTypes.string,
+  starting_date: PropTypes.string,
+  ending_date: PropTypes.string,
   logo: PropTypes.string,
 }
