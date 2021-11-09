381fc4d432171f6b6bee4307efb61006682ccbe9
celiatoc <celiatocqueville@gmail.com>
2021-11-09 11:08:19 +0100

    add students

--- BACK/app/migrations/deploy/dataInsert.sql
+++ BACK/app/migrations/deploy/dataInsert.sql
@@ -92,7 +92,7 @@ INSERT INTO "theuser" ("id", "firstname", "lastname","email","password", "biogra
 (52, 'Quentin', 'Logie', 'quentin@oclock.io', 'oclock9', 'Je suis Professeur', 'https://res.cloudinary.com/dieupu7jn/image/upload/v1636021974/oProjetheque/gateau/cookies_1_uewmdx.png', 1, 9, 1);
 INSERT INTO "theuser" ("id", "firstname", "lastname","email","password", "biography","avatar","id_promo", "id_therole") VALUES
 (53, 'Sophie', 'Fonfeque', 'sophief@oclock.io', 'valhalla44', 'Développeuse Fullstack spécialité Data/API', 'https://res.cloudinary.com/dieupu7jn/image/upload/v1636021974/oProjetheque/gateau/cookies_1_uewmdx.png', 2, 1),
-(53, 'James', 'Bond', 'james@oclock.io', 'valhalla44', 'Développeur Fullstack spécialité Data/API', 'https://res.cloudinary.com/dieupu7jn/image/upload/v1636021974/oProjetheque/gateau/cookies_1_uewmdx.png', 3, 1),
+(54, 'James', 'Bond', 'james@oclock.io', 'valhalla44', 'Développeur Fullstack spécialité Data/API', 'https://res.cloudinary.com/dieupu7jn/image/upload/v1636021974/oProjetheque/gateau/cookies_1_uewmdx.png', 3, 1);
 
 -- PostGres with type serial does not automatically increment the sequence attached to the column!
 -- It is therefore necessary to update the current value of each of the sequences by selecting the maximum id of each table
