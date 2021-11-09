ae390d746964fe09f6aad5755a6b3eb213385ee7
celiatoc <celiatocqueville@gmail.com>
2021-11-09 17:48:11 +0100

    réinstallation

--- BACK/app/models/project.js
+++ BACK/app/models/project.js
@@ -90,10 +90,7 @@ class Project{
         // console.log(result);
         return result.rows;
     }
-<<<<<<< HEAD
-=======
-    
->>>>>>> parent of 1aed69fd (Merge branch 'main' into onsentape)
+
     /* ---------------------------------------------- */
     /**
      * Return new project
