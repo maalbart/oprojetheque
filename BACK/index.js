3faf285516c5c1c47a670c36d523f0f529661fa5
celiatoc <celiatocqueville@gmail.com>
2021-11-10 12:04:44 +0100

    router connection

--- BACK/index.js
+++ BACK/index.js
@@ -81,8 +81,15 @@ const cors = require('cors');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X_Token, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    next();
+    if(req.method === 'OPTIONS'){
+      res.sendStatus(200);
+    }
+    else{
+      next();
+    }
+    
 });
 app.use(cors({
     origin: '*'}));
